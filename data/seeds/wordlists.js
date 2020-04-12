0d37de4cc69f9671e4dca88ef07680da5a65f9ae
Michael Harms <michael.harms6010@gmail.com>
2020-04-12 17:06:13 -0500

    updates seeds

--- data/seeds/wordlists.js
+++ data/seeds/wordlists.js
@@ -1,13 +1,13 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('wordlists').del()
+  return knex('health_data').del()
     .then(function () {
       // Inserts seed entries
-      return knex('wordlists').insert([
-        {approved: true, user_id: 1, title: "Some Words", wordlist: 'the,quick,brown,fox,jumped,over,the,lazy,dog,furthermore,both,were,good,boys'},
-        {approved: true, user_id: 2, title: "Tech Tools", wordlist: 'javascript,html,sass,nodejs,nodemon,express,reactjs,contextapi,jsonwebtoken'},
-        {approved: true, user_id: 3, title: "Names", wordlist: 'brian,michael,emily,kristi,shya,catherine,webpt11,dreamteam'}
+      return knex('health_data').insert([
+        {lost_sense_of_smell: true, user_id: 1, cough: false, tight_chest: false, aches: false},
+        {lost_sense_of_smell: true, user_id: 2, cough: false, tight_chest: false, aches: false},
+        {lost_sense_of_smell: true, user_id: 3, cough: false, tight_chest: false, aches: false}
       ]);
     });
 };
